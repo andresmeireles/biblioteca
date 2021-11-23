f9402fdb7af5a1acee786aff9002502d90ffcf1f
André Meireles <andre2meireles@gamil.com>
2021-11-23 10:18:41 -0300

    adicionar psalm rules for phpunit

--- tests/Unit/Services/Library/AddBookTest.php
+++ tests/Unit/Services/Library/AddBookTest.php
@@ -2,17 +2,64 @@
 
 namespace Tests\Unit\Services\Library;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Book;
+use App\Models\User;
+use App\Services\Library\AddBook;
+use Illuminate\Database\QueryException;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class AddBookTest extends TestCase
 {
+    use RefreshDatabase;
+
+    private AddBook $adder;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        User::factory()->create();
+
+        $this->adder = new AddBook();
+    }
+
     /**
      * A basic unit test example.
      *
      * @return void
      */
-    public function test_example()
+    public function testAddBook(): void
     {
-        $this->assertTrue(true);
+        $data = [
+            'name' => 'nas montanhas da loucura',
+            'author' => 'h. p. lovecraft',
+            'publication_year' => '1996',
+            'code' => '51',
+            'genre' => 'horror',
+            'created_by' => 1,
+        ];
+        $this->adder->add($data);
+
+        $result = Book::all()->count();
+        $this->assertEquals(1, $result);
+    }
+
+    // Error will th
+    public function testErrorOnAddBook(): void
+    {
+        $this->expectException(QueryException::class);
+        $data = [
+            'name' => 'nas montanhas da loucura',
+            'author' => 'h. p. lovecraft',
+            'publication_year' => '1996',
+            'code' => '51',
+            'genre' => 'horror',
+            'created_by' => 1,
+        ];
+        $this->adder->add($data);
+        $this->adder->add($data);
+        
+        $this->assertEquals(2, Book::all()->count());
     }
 }
